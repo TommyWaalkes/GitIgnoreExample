ea77a5e8f9e863d8aa1936618f4f2b3eab9c2025
tommywaalkes <tommy@grandcircus.co>
2022-10-13 13:22:29 -0400

    Added everything else

---
+++ Example/Example/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("This is an example of using a git ignore!!!");
+        }
+    }
+}
